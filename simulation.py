01f25fc19d11b9139b24eda18017eaff79c3050c
Ethan <ethanstabenow@gmail.com>
2026-01-13 23:25:12 -0500

    turned off gui elements

--- simulation.py
+++ simulation.py
@@ -1,6 +1,17 @@
 import pybullet as p
+import time
 
+#set up connect
 physicsClient = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+for i in range(1000):
+    p.stepSimulation()
+
+    time.sleep(1/60)
+    print(i)
+
+
 p.disconnect()
 
 
