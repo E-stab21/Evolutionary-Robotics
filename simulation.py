fdb66c6b3864a1030d3d7812e81c66d2c811d27f
Ethan <ethanstabenow@gmail.com>
2026-02-10 12:41:49 -0500

    sensors part 1

--- simulation.py
+++ simulation.py
@@ -1,6 +1,7 @@
 import generate
 import pybullet as p
 import pybullet_data
+from pyrosim import pyrosim
 import time
 
 #generate
@@ -17,8 +18,12 @@ planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 
+pyrosim.Prepare_To_Simulate(robotId)
+
 for i in range(3000):
     p.stepSimulation()
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    print(backLegTouch)
     time.sleep(1/100)
 p.disconnect()
 
